953c2f15cc99d30db12057338b06880388afdd56
Michele Lacchia <michelelacchia@gmail.com>
2015-12-09 11:04:02 +0100

    Add Perl env vars

--- modules/myalias/init.zsh
+++ modules/myalias/init.zsh
@@ -24,3 +24,9 @@ export PATH=$PATH:~/.local/bin
 export PATH=$PATH:~/Scripts
 # dg
 export PYTHONPATH=$PYTHONPATH:~/dg
+
+export PATH="/home/miki/perl5/bin${PATH+:}${PATH}"; export PATH;
+export PERL5LIB="/home/miki/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"; export PERL5LIB;
+export PERL_LOCAL_LIB_ROOT="/home/miki/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"; export PERL_LOCAL_LIB_ROOT;
+export PERL_MB_OPT="--install_base \"/home/miki/perl5\""; export PERL_MB_OPT;
+export PERL_MM_OPT="INSTALL_BASE=/home/miki/perl5"; export PERL_MM_OPT;
