eb4bb5edc152cca73bbd21696dcd5a090b8f42a4
Michele Lacchia <michelelacchia@gmail.com>
2016-06-21 16:32:29 +0200

    Change theme and move exports

--- modules/environment/init.zsh
+++ modules/environment/init.zsh
@@ -6,6 +6,25 @@
 #
 
 #
+# General settings
+#
+
+export TERM='rxvt-unicode'
+export EDITOR='nvim'
+export PATH=$PATH:/opt/android-sdk/build-tools/23.0.2
+export PATH=$PATH:/opt/android-sdk/platform-tools
+# Ruby gems
+export PATH=$PATH:~/.gem/ruby/2.0.0/bin
+export PATH=$PATH:/root/.gem/ruby/2.0.0/bin
+# Local bins
+export PATH=$PATH:~/.local/bin
+
+export PERL5LIB="/home/miki/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"
+export PERL_LOCAL_LIB_ROOT="/home/miki/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"
+export PERL_MB_OPT="--install_base \"/home/miki/perl5\""
+export PERL_MM_OPT="INSTALL_BASE=/home/miki/perl5"
+
+#
 # Gtk 3 DPI settins
 #
 
--- modules/myalias/init.zsh
+++ modules/myalias/init.zsh
@@ -11,23 +11,3 @@ alias hlint='hlint -c'
 alias ccat='~/exp/bin/pygmentize -O style=fruity -f256 -g'
 alias exe='exercism'
 alias matlab='~/MATLAB2016a/bin/matlab -nodisplay -nosplash -nodesktop'
-
-export TERM='rxvt-unicode'
-export EDITOR='vim'
-export PATH=$PATH:/opt/android-sdk/build-tools/23.0.2
-export PATH=$PATH:/opt/android-sdk/platform-tools
-# Ruby gems
-export PATH=$PATH:~/.gem/ruby/2.0.0/bin
-export PATH=$PATH:/root/.gem/ruby/2.0.0/bin
-# Local bins
-export PATH=$PATH:~/.local/bin
-# Shell scripts
-export PATH=$PATH:~/Scripts
-# dg
-export PYTHONPATH=$PYTHONPATH:~/dg
-
-export PATH="/home/miki/perl5/bin${PATH+:}${PATH}"; export PATH;
-export PERL5LIB="/home/miki/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"; export PERL5LIB;
-export PERL_LOCAL_LIB_ROOT="/home/miki/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"; export PERL_LOCAL_LIB_ROOT;
-export PERL_MB_OPT="--install_base \"/home/miki/perl5\""; export PERL_MB_OPT;
-export PERL_MM_OPT="INSTALL_BASE=/home/miki/perl5"; export PERL_MM_OPT;
